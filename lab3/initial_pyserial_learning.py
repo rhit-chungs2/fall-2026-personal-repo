6664bf481db97bb002118adb42aeca12171353f0
Jason Chung <chungs2@rose-hulman.edu>
2026-09-24 13:29:12 -0400

    updated pyserial

--- lab3/initial_pyserial_learning.py
+++ lab3/initial_pyserial_learning.py
@@ -1,14 +1,27 @@
 import serial
+import time
 
 print("Learning Pyserial")
 
+# ser = serial.Serial(port="/dev/cu.usbmodem21201", baudrate=19200, timeout=10)
+ser = serial.Serial(port="/dev/ttyACM0", baudrate=19200, timeout=10)
 
-ser = serial.Serial("/dev/ttyAMA10",baudrate=19200,timeout=10)
+# time.sleep(2.0)
+time.sleep(1)
 
-while not ser.is_open:
-    print("Opening...")
+ser.reset_input_buffer()
+message = "RESET"
+print(message)
+message_bytes = (message + "\n").encode()
+print(message_bytes)
 
-# TODO: use the ser object
+ser.write(message_bytes)
 
+response_bytes = ser.readline()
+print(response_bytes)
+response = response_bytes.decode().strip()
+print(response)
 
 ser.close()
+
+
