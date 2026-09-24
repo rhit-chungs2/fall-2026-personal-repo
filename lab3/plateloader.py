61c3f363768a85de6958b0620989ae2c2d4e8acc
Jason Chung <chungs2@rose-hulman.edu>
2026-09-24 14:11:46 -0400

    Finish Plate Loader Class

---
+++ lab3/plateloader.py
@@ -0,0 +1,44 @@
+import serial
+import time
+
+
+class PlateLoader:
+    def __init__(self, port="/dev/ttyACM0"):
+        self.port = port
+        self.ser = None
+
+    def connect(self):
+        if self.ser and self.ser.is_open:
+            return
+        self.ser = serial.Serial(port=self.port, baudrate=19200, timeout=15)
+        time.sleep(2.0)
+        self.ser.reset_input_buffer()
+
+
+    def disconnect(self):
+        if self.ser and self.ser.is_open:
+            self.ser.close()
+
+
+    def send_command(self,command):
+        self.ser.reset_input_buffer()
+        message_bytes = (command + "\n").encode()
+        print(message_bytes)
+
+        self.ser.write(message_bytes)
+
+        response_bytes = self.ser.readline()
+        print(response_bytes)
+        response = response_bytes.decode().strip()
+        return response
+    
+if __name__ == "__main__":
+    print("Quick PlateLoader testing")
+    loader = PlateLoader()
+    loader.connect()
+    response = loader.send_command("RESET")
+    print("Respones: ", response)
+    loader.disconnect()
+
+
+
